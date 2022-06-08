871944684d642adbc379c107126973b885176f79
mikhanarhia <99376866+mikhanarhia@users.noreply.github.com>
2022-06-08 12:41:52 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+# import io
+
+
+# from tensorflow import keras
+# from PIL import Image
+
+
+# import cv2
+# import numpy as np
+from flask import Flask, request, jsonify
+
+#Load Model
+# model = keras.models.load_model("my_model.h5")
+# model.compile(loss='categorical_crossentropy',
+#               optimizer='adam',
+#               metrics=['accuracy'])
+
+# def transform_img(x):
+#     img = cv2.imread(x)
+#     img = cv2.resize(img,(150,150))
+#     img = np.reshape(img,[1,150,150,3])
+#     return img
+
+app = Flask(__name__)
+
+@app.route("/", methods= ['POST'])
+def coba():
+    if request.method == "POST":
+
+        #load image dari post request
+        # image = request.files.get('file')
+        data = request.get_json()
+        # if image is None or image.filename == "":
+        #     return jsonify({"error": "no file/image"})
+
+        try:
+            # Mulai prediksi gambar dari model
+            # img_bytes = image.read()
+            # pil_img = Image.open(io.BytesIO(img_bytes))
+            # img = transform_img(pil_img)
+            # cla = model.predict(img)
+            # classes = np.argmax(cla)
+            classes = data['hasil']
+            return jsonify({"hasil" :classes})
+        except Exception as e:
+            return jsonify({"error" : str(e)})
+
+    return "OK"
+
+if __name__ == "__main__":
+    app.run(debug=True)
