05c3cb3270f115e06ac6a9feb4d98cf0e5f29720
Raihan <raihan.hakim2000@gmail.com>
2022-06-11 16:22:03 +0700

    edit json response

--- main.py
+++ main.py
@@ -125,10 +125,10 @@ def predict64():
 
 
     return jsonify({
-    "index-eyebag": int(eb_index),
-    "index-eyelid": int(el_index),
-    "prob-eyebag": eb_prob,
-    "prob-eyelid": el_prob
+    "index_eyebag": int(eb_index),
+    "index_eyelid": int(el_index),
+    "prob_eyebag": eb_prob,
+    "prob_eyelid": el_prob
     })
     # return 'ok'
 
